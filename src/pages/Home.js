d89ecb3dc8f00b9762a9bee852b636d185e8c365
AnkitJhadev <jha80250@gmail.com>
2024-06-19 13:18:56 +0530

    Initialez our react-app and created home page with productlits

---
+++ src/pages/Home.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Navbar from '../features/navbar/navbar'
+import ProductList from '../features/Products/ProductList'
+
+function Home() {
+  return (
+    <div>
+
+        <Navbar>
+            <ProductList/>
+        </Navbar>
+
+
+    </div>
+  )
+}
+
+export default Home
